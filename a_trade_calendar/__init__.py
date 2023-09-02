4f43bc50274894eddeee7d8d9fbf9331f5358f69
汪召军 <wangnewton@163.com>
2023-09-02 10:30:51 +0800

    1

--- a_trade_calendar/__init__.py
+++ a_trade_calendar/__init__.py
@@ -3,4 +3,4 @@
 :Author:  逍遥游
 :Create:  2023/09/01$ 09:51$
 """
-from . import calendar_util
+from calendar_util import is_trade_date, get_latest_trade_date, get_pre_trade_date, get_next_trade_date, get_trade_days_interval
